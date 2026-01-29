724ce765323a63ebef34ad2bf2ee09aecb9badec
ClubKoala <108206543+clubkoala@users.noreply.github.com>
2026-01-29 09:25:21 -0600

    werewr

--- server scripts/mystical-agriculture-recipies.js
+++ server scripts/mystical-agriculture-recipies.js
@@ -1,17 +1,26 @@
 ServerEvents.recipes(event => {
-    event.remove({output: 'mysticalagriculture:tertium_sword'})
-    event.remove({output: 'mysticalagriculture:imperium_sword'})
-    event.remove({output: 'mysticalagriculture:supremium_sword'})
-    event.remove({output: 'mysticalagriculture:awakened_supremium_sword'})
-    event.remove({output: 'mysticalagriculture:insanium_sword'})
-    event.remove({output: 'mysticalagriculture:unobtainium_sword'})
+  let mystblockup = (input, output) => {
+    event.shaped(
+      Item.of(output, 1), // arg 1: output
+      [
+        'AAA',
+        'ABA',
+        'AAA'
+      ],
+      {
+        A: input,
+        B: 'mysticalagriculture:master_infusion_crystal'
+      }
+    ).keepIngredient('mysticalagriculture:master_infusion_crystal').id('kubejs:mystical_agriculture/block_upgrade/' + output.split(':')[1])
+  }
+  mystblockup('mysticalagriculture:inferium_block', 'mysticalagriculture:prudentium_block')
+  mystblockup('mysticalagriculture:prudentium_block', 'mysticalagriculture:tertium_block')
+  mystblockup('mysticalagriculture:tertium_block', 'mysticalagriculture:imperium_block')
+  mystblockup('mysticalagriculture:imperium_block', 'mysticalagriculture:supremium_block')
 })
 
-RecipeViewerEvents.removeEntriesCompletely('item', event => {
-	event.remove('mysticalagriculture:tertium_sword')
-    event.remove('mysticalagriculture:imperium_sword')
-    event.remove('mysticalagriculture:supremium_sword')
-    event.remove('mysticalagriculture:awakened_supremium_sword')
-    event.remove('mysticalagriculture:insanium_sword')
-    event.remove('mysticalagriculture:unobtainium_sword')
-})
+//Tier 1 (Inferium): Basic tier, used to start the mod.
+//Tier 2 (Prudentium): Upgraded from Inferium, used for faster growth and better gear.
+//Tier 3 (Tertium/Intermedium): Middle-tier, offering higher yield.
+//Tier 4 (Imperium): High-tier, used for more advanced resources.
+//Tier 5 (Supremium): Highest tier, provides maximum benefits and powerful tools. 
