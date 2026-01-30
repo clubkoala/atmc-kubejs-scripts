7eb27c8f6141a4f5573dddea08336c6bc4e113aa
ClubKoala <108206543+clubkoala@users.noreply.github.com>
2026-01-30 10:26:15 -0600

    54

---
+++ startup scripts/jjk.js
@@ -0,0 +1,33 @@
+StartupEvents.registry('irons_spellbooks:spells', event => {
+  event.create('Dismantle')
+    .setCastTime(0)
+    .setCooldownSeconds(5)
+    .setManaCostPerLevel(100)
+    .setCastType('instant')
+    .setSchool('irons_spellbooks:eldritch')
+    .setMinRarity('legendary')
+    .setMaxLevel(1)
+    .setFinishSound('item.honey_bottle.drink')
+    .onCast(ctx => global.dismantle(ctx))
+    .setAllowLooting(true)
+    .needsLearning(true)
+    .canBeCraftedBy(player => true)
+    .setUniqueInfo((spellLevel, caster) => {
+      return [
+        Component.green(`Damage: 20`)
+      ]
+    })
+    .checkPreCastConditions(ctx => {
+      return ISSUtils.preCastTargetHelper(ctx.level, ctx.entity, ctx.playerMagicData, ctx.spell, 10, 0.35)
+    })
+})
+
+global.dismantle = (ctx) => {
+  const target = ISSUtils.getSpellTargetEntity(ctx.level, ctx.entity, 10, 0.35)
+  if (target != null) {
+    if (target instanceof EntityLivingBase) {
+      const damage = 20
+      target.attackEntityFrom(DamageSource.causeIndirectMagicDamage(ctx.entity, ctx.entity), damage)
+    }
+  }
+}
